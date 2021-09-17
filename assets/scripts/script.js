82f22be42602c242e1446c13277feb37091903d6
josephdaw <josephdaw@icloud.com>
2021-09-17 14:14:31 +0930

    add prompts for password options

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,62 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const alphabetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const digits = [0,1,2,3,4,5,6,7,8,9];
+const specialChar = ["!","@","#","$","%","&","*","(",")","-"];
+
+
+// create function to generate password
+function generatePassword() {
+  // create an empty array which will be used to hold selected characters
+  var passwordCharacters = [];
+  
+
+  // ask user how long they want their password (between 8 and 128 incl)
+  var passwordLength = prompt("How long would you like your password to be?\nType a number greater than 7 and less than 129");
+
+  // ask user if they want to include lowercase
+  var inclLowerLetters = confirm("Would you like to include lowercase letters?");
+  // if true add to passwordCharacters
+  if (inclLowerLetters){
+    passwordCharacters = passwordCharacters.concat(alphabetLower);
+  };
+
+  // ask user if they want to include uppercase
+  var inclUpperLetters = confirm("Would you like to include uppercase letters?");
+  // if true add to passwordCharacters
+  if (inclUpperLetters){
+    passwordCharacters = passwordCharacters.concat(alphabetUpper);
+  };
+
+  // ask user if they want to include numbers
+  var inclNumbers = confirm("Would you like to include numbers?");
+  // if true add to passwordCharacters
+  if (inclNumbers){
+    passwordCharacters = passwordCharacters.concat(digits);
+  };
+
+  // ask user if they want to include special characters
+  var inclSpecials = confirm("Would you like to include special characters?");
+  // if true add to passwordCharacters
+  if (inclSpecials){
+    passwordCharacters = passwordCharacters.concat(specialChar);
+  };
+
+  // check that the user has selected at least one option
+
+
+  console.log(passwordLength,inclLowerLetters, inclUpperLetters, inclNumbers, inclSpecials);
+  console.log(passwordCharacters);
+
+
+  // generate the password
+
+  // return the password to be writen to page
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
